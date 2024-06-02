7f8324db1164baaba9e1b591b417b37047d364a0
a-kumar5 <ayush.1306.kumar@gmail.com>
2024-06-02 18:55:59 +0530

    WIP: Code changes to get and update depoyment

--- src/main.go
+++ src/main.go
@@ -5,12 +5,15 @@ import (
 	"flag"
 	"fmt"
 
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/retry"
 )
 
 func main() {
+	// code to login into cluster
 	kubeconfig := flag.String("kubeconfig", "/Users/ayushkumar/.kube/config", "location of kubeconfig file")
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
@@ -21,18 +24,43 @@ func main() {
 		panic(err)
 	}
 	ctx := context.Background()
+
+	// Getting pods from default namespace
+
 	pods, err := clientset.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
 	if err != nil {
-		// handle error
+		panic("Error while getting pods list")
 	}
 	for _, pod := range pods.Items {
 		fmt.Printf("%+v\n", pod.Name)
 	}
-	hpa, err := clientset.AutoscalingV2().HorizontalPodAutoscalers("default").List(ctx, metav1.ListOptions{})
+
+	// Getting deployment from default namespace
+
+	deployment, err := clientset.AppsV1().Deployments("default").List(ctx, metav1.ListOptions{})
 	if err != nil {
-		// handle error
+		panic("Error while getting deployment list")
 	}
-	for _, hpa := range hpa.Items {
-		fmt.Printf("%+v\n", hpa.Name)
+	for _, deploy := range deployment.Items {
+		fmt.Printf("%+v\t %+v\n", deploy.Name, *deploy.Spec.Replicas)
 	}
+
+	// Updating replicas of given deployment
+	deploymentClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
+	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		result, getErr := deploymentClient.Get(context.TODO(), "nginx", metav1.GetOptions{})
+		if getErr != nil {
+			panic(fmt.Errorf("Failed to get latest version of Deployment: %v", getErr))
+		}
+		result.Spec.Replicas = int32Ptr(1)
+		result.Spec.Template.Spec.Containers[0].Image = "nginx:1.13" // change nginx version
+		_, updateErr := deploymentClient.Update(context.TODO(), result, metav1.UpdateOptions{})
+		return updateErr
+	})
+	if retryErr != nil {
+		panic(fmt.Errorf("Update failed: %v", retryErr))
+	}
+	fmt.Println("Updated deployment...")
 }
+
+func int32Ptr(i int32) *int32 { return &i }
